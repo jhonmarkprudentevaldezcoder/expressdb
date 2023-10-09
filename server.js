75357ce8dd832fff96bf47b9bf83337992be9f0c
mark valdez <jhonmarkprudentevaldezcoder@gmail.com>
2023-10-09 10:45:23 +0800

    update

--- server.js
+++ server.js
@@ -52,6 +52,21 @@ app.post("/insertschedule", async (req, res) => {
   }
 });
 
+app.delete("/schedule/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const schedule = await Schedules.findByIdAndDelete(id);
+    if (!schedule) {
+      return res
+        .status(404)
+        .json({ message: `cannot find any product with ID ${id}` });
+    }
+    res.status(200).json(schedule);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
 // fetch all checkups
 app.get("/checkups", async (req, res) => {
   try {
